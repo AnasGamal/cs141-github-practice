79788889afe40b30d2e8e7009c2452f62fffdbbc
M3ss1-13 <ldwood@stetson.edu>
2025-04-21 14:15:03 -0400

    Update Main.java
    added my name

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello World");
-		
+		System.out.println("Logan")
 		System.out.println("Anas");
 		System.out.println("EJ");
 		System.out.println("Dr. Scarlato");
